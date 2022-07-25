63a443e388c99542efcd044abcb993544511b29e
Leonardo Hernandez <hernandeez00@gmail.com>
2022-07-25 16:01:27 -0300

    apimock-changes

--- wireMockAPI.py
+++ wireMockAPI.py
@@ -2,8 +2,15 @@
 # 2 - Abrir Git Bash e executar o arquivo .jar baixado com o seguinte comando:
 #       >> java -jar wiremock-jre8-standalone-2.33.2.jar
 
+from ast import parse
 import requests
+import json
 from requests.structures        import CaseInsensitiveDict
+
+def testJsonFile():
+    with open(r'./GitHub/Wiremock-Standalone-MockAPI/_jarStandalone/idreq_2980.json', 'r') as file:
+        parseJson = json.load(file)
+        return json.dumps(parseJson)
 
 def createMock():
     URL = 'http://localhost:8080/__admin/mappings/new'
@@ -11,11 +18,16 @@ def createMock():
     headers = CaseInsensitiveDict()
     headers['Content/Type'] = 'apllication/json'
 
-    data = r'{ "request": { "url": "/get/this", "method": "GET" }, "response": { "status": 200, "body": "Here it is!\n" }}'
-    resp = post(url=URL, data=data)
+    #jsonData = testJsonFile()
 
-    print(f'statu code: {resp.status_code} \n request: {resp.request} \n Aparent encoding: {resp.apparent_encoding} \n Encoding: {resp.encoding} \n Content: {resp.content} \n Cookies: {resp.cookies} \n Elapsed: {resp.elapsed} \n' +
-           f'Headers: {resp.headers} \n History: {resp.history} \n  RAW: {resp.raw} \n Json: {resp.json} \n Links: {resp.links} \n')
+    #data = r'{ "request": { "url": "/get/this", "method": "GET" }, "response": { "status": 200, "bodyFileName": "' + f'{jsonData}' + '"}}'
+    data = r'{ "request": { "url": "/get/this", "method": "GET" }, "response": { "status": 200, "bodyFileName": "/idreq.json" }}'
+    #data = r'{ "request": { "url": "/get/this", "method": "GET" }, "response": { "status": 200, "body": "Here it is!\n" }}'
+
+    resp = requests.post(url=URL, data=data)
+
+    print(f' Status code: {resp.status_code} \n request: {resp.request} \n Aparent encoding: {resp.apparent_encoding} \n Encoding: {resp.encoding} \n Content: {resp.content} \n Cookies: {resp.cookies} \n Elapsed: {resp.elapsed} \n' +
+           f' Headers: {resp.headers} \n History: {resp.history} \n RAW: {resp.raw} \n Json: {resp.json} \n Links: {resp.links} \n')
 
 def testMock():
     URL = 'http://localhost:8080/get/this'
@@ -23,9 +35,13 @@ def testMock():
     headers = CaseInsensitiveDict()
     headers['Content-Type'] = 'application/json'
 
-    resp = requests.get(url=URL)
+    resp = requests.get(url=URL, headers=headers)
     
-    print(resp.content)
+    jsonData = json.dumps(resp.json(), indent=4)
+    testeDict = dict(resp.json())
+
+    print(testeDict['data']['idCotacao'])
+    #print(json.dumps(resp.json(), indent=4))
 
 def myTestMock():
     URL = 'http://localhost:8080/api/mytest'
@@ -42,7 +58,7 @@ def shutDownWireMock():
 
     print(f"{resp.content} \n {resp.status_code}")
 
-createMock()
-#testMock()
+#createMock()
+testMock()
 #myTestMock()
 #shutDownWireMock()
